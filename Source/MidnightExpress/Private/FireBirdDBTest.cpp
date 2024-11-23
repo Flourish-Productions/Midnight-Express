d0c6e533f6e189b08515cd8e0668d5068c181951
Eden <ecoding@pm.me>
2024-11-22 23:57:02 -0800

    Created FirebirdDB test funciton
    Purpose is to push data to temp Firebird database

---
+++ Source/MidnightExpress/Private/FireBirdDBTest.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FireBirdDBTest.h"
+
+FireBirdDBTest::FireBirdDBTest()
+{
+	int databaseNum = 0;
+}
+
+FireBirdDBTest::~FireBirdDBTest()
+{
+}
